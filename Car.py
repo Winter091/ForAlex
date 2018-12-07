385da36b3554d9f8a3d1e32749b8fc6153c7c1da
Winter091 <33420667+Winter091@users.noreply.github.com>
2018-12-07 17:55:02 +0300

    Add files via upload

--- Car.py
+++ Car.py
@@ -2,36 +2,36 @@ import pygame as pg
 
 
 class Car():
-	def __init__(self, size: tuple, start_coords: tuple, speed: int, color: tuple):
+    def __init__(self, size: tuple, start_coords: tuple, speed: int, color: tuple):
 
-		# Текстурка предмета
-		self.texture = pg.Surface(size)
-		self.texture.fill(color)
+        # Текстурка предмета
+        self.texture = pg.Surface(size)
+        self.texture.fill(color)
 
-		# Для проверки коллизий используется невидимый прямоугольник
-		self.rect = pg.Rect(start_coords, size)
+        # Для проверки коллизий используется невидимый прямоугольник
+        self.rect = pg.Rect(start_coords, size)
 
-		self.speed = speed
+        self.speed = speed
 
-	def update(self, core):
-		"""
-		В эту функцию передается главный класс программы - класс core.
-		Это необходимо для того, чтобы данному методу были доступны
-		абсолютно все части и классы программы. Сейчас это не обязательно,
-		тут всё очень просто, но дальше, если будет прописываться коллизия
-		с другими предметами, это очень поможет.
-		"""
+    def update(self, core):
+        """
+        В эту функцию передается главный класс программы - класс core.
+        Это необходимо для того, чтобы данному методу были доступны
+        абсолютно все части и классы программы. Сейчас это не обязательно,
+        тут всё очень просто, но дальше, если будет прописываться коллизия
+        с другими предметами, это очень поможет.
+        """
 
-		if core.keyR:
-			self.rect.x += self.speed
-		if core.keyL:
-			self.rect.x -= self.speed
-		if core.keyU:
-			self.rect.y -= self.speed
-		if core.keyD:
-			self.rect.y += self.speed
+        if core.keyR:
+            self.rect.x += self.speed
+        if core.keyL:
+            self.rect.x -= self.speed
+        if core.keyU:
+            self.rect.y -= self.speed
+        if core.keyD:
+            self.rect.y += self.speed
 
-	def render(self, screen):
+    def render(self, screen):
 
-		# Текстура данного объекта помещается на экран
-		screen.blit(self.texture, (self.rect.x, self.rect.y))
+        # Текстура данного объекта помещается на экран
+        screen.blit(self.texture, (self.rect.x, self.rect.y))
